c3d1430a755c57fb1ab0b7854440ef5eca141820
cxy19892 <cxy19892@foxmail.com>
2020-04-16 14:54:13 +0800

    threed commit

--- jump7.py
+++ jump7.py
@@ -1,10 +1,11 @@
 for a in range(1, 100):
-    if a % 7 == 0:
-	continue
-    elif a / 10 == 7:
-	continue
-    elif a % 10 == 7:
-	continue
-    else:
-	print(a)
+	if a % 7 == 0:
+		continue	
+	elif a / 10 == 7:
+		continue
+	elif a % 10 == 7:
+		continue
+	else:
+		print(a)
+print('print over')
 
